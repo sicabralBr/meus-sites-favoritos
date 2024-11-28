95276e43140acba8412509a3c65ac27ce91cdd24
Simone <simonecabral.br@gmail.com>
2024-11-28 19:01:28 -0300

    Add cadastro de sites

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("------- MEUS SITES FAVORITOS -------");
+
+        Scanner scanner = new Scanner(System.in);
+        List<Website> meusWebsites = new ArrayList<>();
+
+        System.out.println("Informar o nome do site: ");
+        String nome_site = scanner.nextLine();
+        System.out.println("Informar a url do site: ");
+        String url_site = scanner.nextLine();
+
+        Website website = new Website();
+        website.setNome_site(nome_site);
+        website.setUrl_site(url_site);
+
+        meusWebsites.add(website);
     }
 }
